92f8ec1f71e7d956c1985c3dfe59af0ce819f356
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-08 01:59:14 +0600

    about-me-added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from './About';
 import { Header } from './Header';
 import Service from './Service';
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Header/>
             <Service/>
+            <About/>
         </div>
     );
 };
