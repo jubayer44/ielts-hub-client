8821f196fd28f2298517539505bf990f84c7c50f
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-08 01:01:00 +0600

    home-page-service-added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './Header';
+import Service from './Service';
 
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Service/>
         </div>
     );
 };
