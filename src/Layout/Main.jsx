e606daa0e7e9cf717c2e64fae14d0b612903863f
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-08 00:48:09 +0600

    nav-header-footer-added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from '../Pages/Shared/Navbar/Navbar';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
